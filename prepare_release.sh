0039a35fbbb252c423f07cb5c141f48afe1b5549
Jeffrey Klardie <j.klardie@amsterdam.nl>
2019-10-30 17:03:31 +0100

    Automatically add git changes

--- prepare_release.sh
+++ prepare_release.sh
@@ -6,6 +6,9 @@ if [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ]
   then echo First arg must be major, minor or patch; exit
 fi
 
+current_version=$(bump2version --dry-run --list $1 | grep "current_version=" | sed -r s,"^.*=",,) || exit;
+changes=$(git log $current_version..HEAD --pretty=format:'- %s')
+
 next_version=$(bump2version --list $1 | grep "new_version=" | sed -r s,"^.*=",,) || exit;
 today=$(date +'%d-%m-%Y')
 
@@ -15,7 +18,7 @@ cat > Changelog.md <<EOF
 ## ${next_version} (${today})
 
 ### Note worthy changes
-- ...
+$changes
 
 ### Backwards incompatible changes
 - ...
