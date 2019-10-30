50e0a6400004e1e570777c3aef08ee53e16233c2
Jeffrey Klardie <j.klardie@amsterdam.nl>
2019-10-30 14:36:58 +0100

    Created failing testcases when audit log does not exist

--- tests/rest_framework/test_viewsets.py
+++ tests/rest_framework/test_viewsets.py
@@ -85,6 +85,14 @@ class TestAuditLogReadOnlyViewset(TestCase):
             mocked_logger.set_results.assert_called_with('test')
             mocked_logger.info.assert_called_with("Retrieve User")
 
+    @mock.patch('rest_framework.mixins.RetrieveModelMixin.retrieve')
+    def test_retrieve_missing_audit_log(self, mocked_retrieve):
+        mocked_retrieve.return_value = Response(data='test')
+        view_set = DynamicViewSet(queryset=User.objects.all())
+        request = self.factory.get('/')
+        response = view_set.retrieve(request)
+        self.assertEqual(response.data, 'test')
+
     @mock.patch('rest_framework.mixins.ListModelMixin.list')
     def test_list_without_results_with_filter(self, mocked_list):
         mocked_list.return_value = Response(data=['test1', 'test2'])
@@ -114,6 +122,14 @@ class TestAuditLogReadOnlyViewset(TestCase):
             mocked_logger.info.assert_called_with('List User')
             mocked_logger.set_results.assert_called_with(['test1', 'test2'])
 
+    @mock.patch('rest_framework.mixins.ListModelMixin.list')
+    def test_list_missing_audit_log(self, mocked_list):
+        mocked_list.return_value = Response(data=['test1', 'test2'])
+        view_set = DynamicViewSet(queryset=User.objects.all())
+        request = self.factory.get('/')
+        response = view_set.list(request)
+        self.assertEqual(response.data, ['test1', 'test2'])
+
 
 class TestAuditLogViewSet(TestCase):
 
@@ -133,6 +149,14 @@ class TestAuditLogViewSet(TestCase):
             mocked_logger.set_results.assert_called_with('test')
             mocked_logger.info.assert_called_with("Created User object")
 
+    @mock.patch('rest_framework.mixins.CreateModelMixin.create')
+    def test_create_missing_audit_log(self, mocked_create):
+        mocked_create.return_value = Response(data='test', status=201, headers=[])
+        view_set = DynamicViewSet(queryset=User.objects.all())
+        request = self.factory.get('/')
+        response = view_set.create(request)
+        self.assertEqual(response.data, 'test')
+
     @mock.patch('rest_framework.mixins.UpdateModelMixin.update')
     def test_update(self, mocked_update):
         mocked_update.return_value = Response(data='test')
@@ -146,6 +170,14 @@ class TestAuditLogViewSet(TestCase):
             mocked_logger.set_filter.assert_called_with(object_name='User', kwargs={'pk': ''})
             mocked_logger.set_results.assert_called_with('test')
             mocked_logger.info.assert_called_with("Update of User")
+
+    @mock.patch('rest_framework.mixins.UpdateModelMixin.update')
+    def test_update_missing_audit_log(self, mocked_update):
+        mocked_update.return_value = Response(data='test')
+        view_set = DynamicViewSet(queryset=User.objects.all(), kwargs={}, lookup_field='pk')
+        request = self.factory.get('/')
+        response = view_set.update(request)
+        self.assertEqual(response.data, 'test')
 
     @mock.patch('rest_framework.mixins.DestroyModelMixin.destroy')
     def test_destroy(self, mocked_destroy):
@@ -161,3 +193,12 @@ class TestAuditLogViewSet(TestCase):
             mocked_logger.set_filter.assert_called_with(object_name='User', kwargs={'pk': ''})
             mocked_logger.set_results.assert_called_with(None)
             mocked_logger.info.assert_called_with("Destroy User")
+
+    @mock.patch('rest_framework.mixins.DestroyModelMixin.destroy')
+    def test_destroy_missing_audit_log(self, mocked_destroy):
+        mocked_destroy.return_value = Response(status=204)
+        view_set = DynamicViewSet(queryset=User.objects.all(), kwargs={}, lookup_field='pk')
+        request = self.factory.get('/')
+        response = view_set.destroy(request)
+        self.assertEqual(response.data, None)
+        self.assertEqual(response.status_code, 204)
